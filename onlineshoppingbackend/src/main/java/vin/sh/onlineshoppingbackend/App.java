22cc7c5d8baef683a924ec1df663d7371633e624
BDISYS\vinod.sharma <vinod.sharma@powerschool.com>
2017-10-12 23:38:40 +0530

    Done Project Setup

---
+++ onlineshoppingbackend/src/main/java/vin/sh/onlineshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package vin.sh.onlineshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
